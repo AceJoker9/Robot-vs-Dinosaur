c0d2cfddb0323fad38f7d24913bf646508528747
unknown <neilmagic25@gmail.com>
2023-01-26 11:43:59 -0500

    added some more code for the assignment

--- Battlefield.py
+++ Battlefield.py
@@ -1,2 +1,11 @@
 class Battlefied:
 def __init__(self): 
+
+def run_game(self): void
+
+def display_welcome(self): void 
+
+def battle_phase(self): void
+
+def display_winner(self): void 
+
--- Robot.py
+++ Robot.py
@@ -3,6 +3,11 @@
 class Robot: 
     def __init__(self, name):
         self.name = name 
+        self.robt_1health = 1000
+        self.laser_beam = 100
+        
+
+
 
 
 
